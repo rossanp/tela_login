1a7fbdb5cb474bcc1560227bfef9114a0c6f2e1b
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-11-19 17:33:52 -0300

    Created backend

--- tela-de-login/index.js
+++ tela-de-login/index.js
@@ -3,16 +3,20 @@ var user = [
         id: 1,
         nome: "Rossan Pereira",
         email: "rossan@gmail.com",
-        img: "https://otakukart.com/wp-content/uploads/2021/02/aogiri-kaneki-kagune.jpg"
+        img: "https://otakukart.com/wp-content/uploads/2021/02/aogiri-kaneki-kagune.jpg",
+        password: "123456",
     }
 ]
 
 function Enviar() {
     var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+    var imagem = document.getElementById("img-user");
 
-    if ( email == user.email) {
-        console.log("Email correto!")
+    if ( email === user[0].email && pass == user[0].password) {
+        imagem.innerHTML = `<img src='${user[0].img}' class='img-user' alt='Imagem User'>`
+        console.log("Dados corretos!");
     } else {
-        console.log("Email não encontrado!");
+        console.log("Email não encontrado ou senha incorreta!");
     }
 }
