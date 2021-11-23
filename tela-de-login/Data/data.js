f197602ac523a15a366fccf5c6545caa3629cdc6
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-11-23 13:24:02 -0300

    Delete data.js

--- tela-de-login/Data/data.js
+++
@@ -1,8 +0,0 @@
-var user = [
-    {
-        id: 1,
-        nome: "Rossan Pereira",
-        email: "rossan@gmail.com",
-        img: "https://otakukart.com/wp-content/uploads/2021/02/aogiri-kaneki-kagune.jpg"
-    }
-]
