28cccca4d8ab74e2197b9a15c852615c61739bf8
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-11-23 11:44:03 -0300

    Created screen animation Login > Create

--- tela-de-login/src/Script/index.js
+++ tela-de-login/src/Script/index.js
@@ -112,6 +112,10 @@ function Criar() {
     var newPass = document.getElementById("create_pass").value
     var nextId = 0
     var newUsuario = []
+    var boxGeral = document.getElementById("box")
+    var container = document.getElementById("container")
+    var telaCreate = document.getElementById("container-create")
+    var telaLogin = document.getElementById("container-login")
 
     /* Descobrindo o novo id */
     for (i = 0; i <= user.length; i++) {
@@ -121,7 +125,7 @@ function Criar() {
     /* Verificando se o dados existem para adiciona-los */
     if (newNome === "" || newEmail === "" || newPass === "") {
         alert("Favor, preencher os dados.")
-    } else if (VerificarNome() === true || VerificarEmail() === true || VerificarPassword() === true) {
+    } else if (VerificarNomeCreate() === true || VerificarEmailCreate() === true || VerificarPasswordCreate() === true) {
         console.log("Dados incorretos.")
     } else {
         newUsuario = {
@@ -132,7 +136,25 @@ function Criar() {
             password: newPass,
         }
         user.push(newUsuario)
-        console.log("Usuário criado.")
+        /* alert("Usuário criado.") */
+
+        telaCreate.style.display = "none";
+        telaLogin.style.display = "block";
+
+        boxGeral.style.transform = "rotateY(180deg)";
+
+        container.style.transform = "rotateY(180deg)";
+        container.style.transition = "1.2s";
+        container.style.transformStyle = "preserve-3d";
+        container.style.position = "relative";
+
+        telaLogin.style.top = "0";
+        telaLogin.style.left = "0";
+        telaCreate.style.zIndex = "0";
+
+        telaCreate.style.zIndex = "2";
+        telaCreate.style.top = "0";
+        telaCreate.style.left = "0";
     }
 
     console.log(user)
@@ -140,14 +162,63 @@ function Criar() {
 
 /* Mudança de tela Login e Criar */
 function CriarUsuario() {
+    var boxGeral = document.getElementById("box")
+    var container = document.getElementById("container")
     var telaCreate = document.getElementById("container-create")
     var telaLogin = document.getElementById("container-login")
 
-    if ( telaCreate.style.display == "none") {
+    if (telaCreate.style.display == "none") {
         telaCreate.style.display = "block";
         telaLogin.style.display = "none";
-    } else {
+
+        boxGeral.style.transform = "rotateY(180deg)";
+
+        container.style.transform = "rotateY(180deg)";
+        container.style.transition = "1.2s";
+        container.style.transformStyle = "preserve-3d";
+        container.style.position = "relative";
+
+        /* telaLogin.style.backfaceVisibility = "hidden"; */
+        /* telaLogin.style.position = "absolute"; */
+        telaLogin.style.top = "0";
+        telaLogin.style.left = "0";
+        telaLogin.style.zIndex = "2";
+
+        /* telaCreate.style.backfaceVisibility = "hidden"; */
+        /* telaCreate.style.position = "absolute"; */
+        telaCreate.style.top = "0";
+        telaCreate.style.left = "0";
+    }
+}
+
+
+/*  else {
         telaCreate.style.display = "none";
         telaLogin.style.display = "block";
     }
-}
+
+transform: rotateY(180deg);
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+
+    define a velocidade da transição
+.flipper {
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+  }
+
+  esconde o verso durante a animação
+  .front, .back {
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+   left: 0;
+  }
+
+  frente posicionada sobre o verso
+  .front { z-index: 2;  }
+
+  verso inicialmente escondido
+  .back { transform: rotateY(180deg); }*/
